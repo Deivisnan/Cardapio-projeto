90b859ead4d6d9fb7834cb84f5bff0cedc576774
Deivisnan da Silva Bispo <bispodeivisnan@gmail.com>
2024-11-21 17:41:12 -0300

    usando o local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,21 @@
 // src/store/index.js
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
 
 export default createStore({
     state: {
-        carrinho: [],
+        // Carrega o carrinho do Local Storage ou inicia vazio
+        carrinho: JSON.parse(localStorage.getItem("carrinho")) || [],
     },
     mutations: {
         adicionarAoCarrinho(state, item) {
             state.carrinho.push(item);
+            // Atualiza o Local Storage
+            localStorage.setItem("carrinho", JSON.stringify(state.carrinho));
         },
         removerDoCarrinho(state, index) {
             state.carrinho.splice(index, 1);
+            // Atualiza o Local Storage
+            localStorage.setItem("carrinho", JSON.stringify(state.carrinho));
         },
     },
     getters: {
